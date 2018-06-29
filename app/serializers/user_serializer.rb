db5bb85ae6d337143fc6c5732902d934342de438
swholm19 <swholm19@gmail.com>
2018-06-29 11:01:58 -0400

    Feature: user API output includes players
    -Added players to the output for the user API so you can
    see which players belong to the user making the request

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :name, :team_name, :team_statement
+  attributes :id, :email, :name, :team_name, :team_statement, :players
 end
