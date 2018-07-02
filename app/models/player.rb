bb850e42102e38c07814a2f8735eefe7c28e8f15
swholm19 <swholm19@gmail.com>
2018-07-02 16:42:34 -0400

    Removed Keeper Attribute as need Validation
    -For the keeper attribute in the Player resource removed from
    needing validation because it is being done on the front end side.
    Also causing issue because it was boolean so false was valid
    input but when check presence is true it does not work.

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Player < ApplicationRecord
-  validates :name, :position, :keeper, presence: true
+  validates :name, :position, presence: true
   belongs_to :user
 end
--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class PlayerSerializer < ActiveModel::Serializer
   attributes :id, :name, :position, :keeper, :user_id, :roster_spot
 end
